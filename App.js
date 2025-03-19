449d986c4fd3cf2f11585bc83fe3323056c56dab
Johnny Dong <johnny.dong@up.education>
2025-03-19 13:03:21 +1300

    Add React Navigation container + stack

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen'
 import "./global.css";
 
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View className="bg-blue-500 flex-1 items-center justify-center">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}} >
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
---
+++ screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const LoginScreen = () => {
+  return (
+    <View className="items-center justify-center flex-1 bg-blue-500">
+      <Text>Login Screen</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
